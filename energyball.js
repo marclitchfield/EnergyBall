5f4fc658f6389723809ed7827522a703a0e5e8bd
Marc Litchfield <marclitchfield@gmail.com>
2013-01-17 22:32:34 -0800

    reorder methods

--- energyball.js
+++ energyball.js
@@ -1,6 +1,9 @@
 var spheroModule = require('node-sphero');
 var sphero = new spheroModule.Sphero();
 
+var energy = 0;
+var lastX, lastY;
+
 sphero.on('connected', function() {
 	sphero.setStabilization(false);
 	sphero.setDataStreaming([
@@ -8,14 +11,6 @@ sphero.on('connected', function() {
 		sphero.sensors.accelerometer_y
 	]);
 });
-
-var energy = 0;
-var lastX, lastY;
-
-setInterval(function() {
-	sphero.setRGBLED(0, 0, energy, false);
-	energy = Math.max(energy - 0.2, 0);
-}, 1);
 
 sphero.on('notification', function(message) {
 	var x = message.DATA.readInt16BE(0);
@@ -37,3 +32,9 @@ sphero.on('notification', function(message) {
 });
 
 sphero.connect();
+
+
+setInterval(function() {
+	sphero.setRGBLED(0, 0, energy, false);
+	energy = Math.max(energy - 0.2, 0);
+}, 1);
