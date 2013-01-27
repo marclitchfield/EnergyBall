c580b4409b522dab84f5208f974a60548e9a8540
Marc Litchfield <marclitchfield@gmail.com>
2013-01-26 17:46:57 -0800

    Fixed distance formula

--- energyball.js
+++ energyball.js
@@ -3,12 +3,7 @@ var sphero = new spheroModule.Sphero();
 
 var energy = 0;
 var last;
-
-Math.cbrt = function(x) {
-    var sign = x === 0 ? 0 : x > 0 ? 1 : -1;
-    return sign * Math.pow(Math.abs(x), 1/3);
-}
-
+        
 sphero.on('connected', function() {
         sphero.setStabilization(false);
         sphero.setBackLED(0);
@@ -30,7 +25,7 @@ sphero.on('notification', function(message) {
                 var dx = accel.x - last.x;
                 var dy = accel.y - last.y;
                 var dz = accel.z - last.z;
-                var dist = Math.cbrt(dx*dx*dx + dy*dy*dy + dz*dz*dz);
+                var dist = Math.sqrt(dx*dx + dy*dy + dz*dz);
 
                 if (dist > 100) {
                         energy = Math.min(energy + dist/400, 255);
